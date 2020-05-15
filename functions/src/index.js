03903bd6ec6bfe3d63433834934be44f154487dc
Ethan Stillman <ehs.stillman@gmail.com>
2020-05-15 16:41:13 -0700

    Add: Emulator and deployment scripts

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,13 +1,9 @@
+// TODO Load .env vars (?)
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 
 
-// const functions = require('firebase-functions');
-
 const APP = admin.initializeApp()
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
 
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
